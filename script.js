79a0aba8fff85bd13ddd22059f6953bc5a938978
CraftyPlayerYT <maximemoreau100@outlook.fr>
2026-03-17 18:59:07 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
+const SERVER_URL = 'https://dailytask-backend.onrender.com';
 
+async function verifierServeur() {
+  try {
+    const response = await fetch(SERVER_URL);
+    const texte = await response.text();
+    console.log("Statut du Backend :", texte);
+  } catch (error) {
+    console.error("Impossible de joindre le serveur :", error);
+  }
+}
+
+// On lance la vérification au démarrage
+verifierServeur();
